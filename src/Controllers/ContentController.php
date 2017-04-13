f77c31cc59113c985e427198a27712d79d593d9a
schyy <b.kosinski@azupedia.com>
2017-04-13 13:56:16 +0200

    fix

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -5,52 +5,11 @@ namespace Test\Controllers;
 
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
-use Plenty\Modules\Item\DataLayer\Contracts\ItemDataLayerRepositoryContract;
 
 class ContentController extends Controller
 {
     public function sayHello(Twig $twig):string
     {
-      $itemColumns = [
-          'itemDescription' => [
-              'name1',
-              'description'
-          ],
-          'variationBase' => [
-              'id'
-          ],
-          'variationRetailPrice' => [
-              'price'
-          ],
-          'variationImageList' => [
-              'path',
-              'cleanImageName'
-          ]
-      ];
-
-      $itemFilter = [
-          'itemBase.isStoreSpecial' => [
-              'shopAction' => [3]
-          ]
-      ];
-
-      $itemParams = [
-          'language' => 'en'
-      ];
-
-      $resultItems = $itemRepository
-          ->search($itemColumns, $itemFilter, $itemParams);
-
-      $items = array();
-      foreach ($resultItems as $item)
-      {
-          $items[] = $item;
-      }
-      $templateData = array(
-          'resultCount' => $resultItems->count(),
-          'currentItems' => $items
-      );
-
-      return $twig->render('Test::content.hello', $templateData)
+      return $twig->render('Test::content.hello')
     }
 }
